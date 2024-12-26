7ccca4ec4349e81759a00e73be36e90683e250cd
kombe <kombe.kampanga@hotmail.com>
2024-12-26 15:36:51 +1300

    Get next and prev buttons working correctly

--- frontend/WardrobeApp.tsx
+++ frontend/WardrobeApp.tsx
@@ -22,11 +22,21 @@ export default function WardropeApp(props: { toggle: any; }) {
     };
 
     // load wardrobe
-    const [wardrobe, setWardrobe] = useState<string[]>([]);
-    const [tops, setTops] = useState<string[]>([]);
-    const [bottoms, setBottoms] = useState<string[]>([]);
-    const [currentTop, setCurrentTop] = useState<string>("");
-    const [currentBottom, setCurrentBottom] = useState<string>("");
+    const [completeWardrobe, setCompleteWardrobe] = useState<string[]>([]);
+    const [currentWardrobeSelection, setCurrentWardrobeSelection] = useState<string[]>(completeWardrobe);
+    const [currentTops, setCurrentTops] = useState<string[]>([]);
+    const [currentBottoms, setCurrentBottoms] = useState<string[]>([]);
+    const [currentShoes, setCurrentShoes] = useState<string[]>([]);
+    const [currentTopIndex, setCurrentTopIndex] = useState<number>(0);
+    const [currentBottomIndex, setCurrentBottomIndex] = useState<number>(0);
+    const [currentShoeIndex, setCurrentShoeIndex] = useState<number>(0);
+    const [disableTopNextButton, setDisableTopNextButton] = useState<boolean>(true);
+    const [disableTopPrevButton, setDisableTopPrevButton] = useState<boolean>(true);
+    const [disableBottomNextButton, setDisableBottomNextButton] = useState<boolean>(true);
+    const [disableBottomPrevButton, setDisableBottomPrevButton] = useState<boolean>(true);
+    const [disableShoeNextButton, setDisableShoeNextButton] = useState<boolean>(true);
+    const [disableShoePrevButton, setDisableShoePrevButton] = useState<boolean>(true);
+
     const fileInputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
@@ -35,17 +45,29 @@ export default function WardropeApp(props: { toggle: any; }) {
           const response = await axios.get('http://localhost:5000/getExistingWardrobe')
   
           console.log('Response:', response.data);
-
+  
           const allClothes = response.data;
-          setWardrobe(allClothes);
+          setCompleteWardrobe(allClothes);
+          setCurrentWardrobeSelection(allClothes);
           const allTops = allClothes.filter(item => item.toLowerCase().includes("top"))
           const allBottoms = allClothes.filter(item => item.toLowerCase().includes("bottom"))
-          const firstTop = allTops[0]
-          const firstBottom = allBottoms[0]
-          setBottoms(allBottoms);
-          setTops(allTops);
-          setCurrentTop(firstTop)
-          setCurrentBottom(firstBottom);
+          const allShoes = allClothes.filter(item => item.toLowerCase().includes("shoe"))
+          setCurrentBottoms(allBottoms);
+          setCurrentTops(allTops);
+          setCurrentShoes(allShoes);
+          setCurrentTopIndex(0)
+          setCurrentBottomIndex(0);
+          setCurrentShoeIndex(0);
+  
+          if (allTops.length > 1){
+            setDisableTopNextButton(false);
+          };
+          if (allBottoms.length > 1){
+            setDisableBottomNextButton(false);
+          };
+          if (allShoes.length > 1){
+            setDisableShoeNextButton(false);
+          };
   
         }
         catch(error) {
@@ -53,16 +75,118 @@ export default function WardropeApp(props: { toggle: any; }) {
           alert(error.response.data);
         }
       }
+      
       fetchWardrobe();
     }, []);
+
+    const getNextPhoto = (clothingType: string) => {
+      // based on clothing type edit different next states
+      switch (clothingType) {
+        case "top":
+          const updatedTopIndex = currentTopIndex + 1;
+          setCurrentTopIndex(updatedTopIndex)
+          // if this is now the last image, disable the next button
+          if (updatedTopIndex == (currentBottoms.length - 1)){
+            setDisableTopNextButton(true);
+          }
+
+          // if this is no longer the first image, enable the previous button
+          if (updatedTopIndex > 0){
+            setDisableTopPrevButton(false);
+          }
+          break;
+
+        case "bottom":
+          const updatedBottomIndex = currentBottomIndex + 1;
+          setCurrentBottomIndex(updatedBottomIndex)
+
+          // if this is now the last image, disable the next button
+          if (updatedBottomIndex == (currentBottoms.length - 1)){
+            setDisableBottomNextButton(true);
+          }
+
+          // if this is no longer the first image, enable the previous button
+          if (updatedBottomIndex > 0){
+            setDisableBottomPrevButton(false);
+          }
+          break;
+
+        case "shoes":
+          const updatedShoeIndex = currentShoeIndex + 1;
+          setCurrentShoeIndex(updatedShoeIndex)
+          // if this is now the last image, disable the next button
+          if (updatedShoeIndex == (currentShoes.length - 1)){
+            setDisableShoeNextButton(true);
+          }
+
+          // if this is no longer the first image, enable the previous button
+          if (updatedShoeIndex > 0){
+            setDisableShoePrevButton(false);
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    const getPrevPhoto = (clothingType: string) => {
+        switch (clothingType) {
+        case "top":
+          const updatedTopIndex = currentTopIndex - 1;
+          setCurrentTopIndex(updatedTopIndex)
+          // if this is now the first image, disable the prev button
+          if (updatedTopIndex == 0){
+            setDisableTopPrevButton(true);
+          }
+
+          // if this is no longer the last image, enable the next button
+          if (updatedTopIndex < (currentTops.length - 1)){
+            setDisableTopNextButton(false);
+          }
+          break;
+
+        case "bottom":
+          const updatedBottomIndex = currentBottomIndex - 1;
+          setCurrentBottomIndex(updatedBottomIndex)
+          // if this is now the first image, disable the prev button
+          if (updatedBottomIndex == 0){
+            setDisableBottomPrevButton(true);
+          }
+
+          // if this is no longer the last image, enable the next button
+          if (updatedBottomIndex < (currentBottoms.length - 1)){
+            setDisableBottomNextButton(false);
+          }
+          break;
+
+        case "shoes":
+          const updatedShoeIndex = currentShoeIndex - 1;
+          setCurrentShoeIndex(updatedShoeIndex)
+          // if this is now the first image, disable the prev button
+          if (currentShoeIndex == 0){
+            setDisableShoePrevButton(true);
+          }
+
+          // if this is no longer the last image, enable the next button
+          if (updatedShoeIndex < (currentShoes.length - 1)){
+            setDisableShoeNextButton(false);
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
     
-      
+    // TODO: FIX LATER  
     const handleDropdownSelect = () => {
       if (fileInputRef.current) {
         fileInputRef.current.click(); // Trigger file explorer
       }
     };
   
+    // TODO: FIX LATER 
     const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
       const files = event.target.files;
       console.log(files)
@@ -76,7 +200,7 @@ export default function WardropeApp(props: { toggle: any; }) {
             fileNames,
           });
           console.log("Fetched clothes: ", response)
-          setCurrentTop(response.data.firstTopPath);
+          setCurrentTopIndex(response.data.firstTopPath);
         } catch (error) {
           console.error("Error uploading files:", error);
         }
@@ -105,7 +229,7 @@ export default function WardropeApp(props: { toggle: any; }) {
               name: "File",
               list: (
                 <List width="200px" className="dropdown-menu">
-                  <List.Item key="exit-item" onClick={handleDropdownSelect}>
+                  <List.Item key="exit-item" >
                     Load Wardrobe
                       <input
                       ref={fileInputRef}
@@ -146,13 +270,13 @@ export default function WardropeApp(props: { toggle: any; }) {
             >
               <Frame h="100%" bgColor="white" boxShadow="$in" overflow="auto" className='outfit-frame'>
                 <div className='clothing-element-frame'>
-                    <Button>Prev</Button>
+                    <Button disabled={disableTopPrevButton} onClick={() => getPrevPhoto('top')}>Prev</Button>
                     <div className='tops-frame'>
                         <Frame w="100%" h="100%" bgColor="$material" boxShadow="$out" padding="$4">
-                            <img className='clothing-image' src={`http://localhost:5000/wardrobe/${currentTop}`} alt="top" />
+                            <img className='clothing-image' src={`http://localhost:5000/wardrobe/${currentTops[currentTopIndex]}`} alt="top" />
                         </Frame>
                     </div>
-                    <Button>Next</Button>
+                    <Button disabled={disableTopNextButton} onClick={() => getNextPhoto('top')}>Next</Button>
 
                     {/* <div className='jackets-frame'>
                         <Frame w="100%" h="100%" bgColor="$material" boxShadow="$out" padding="$4">
@@ -162,23 +286,23 @@ export default function WardropeApp(props: { toggle: any; }) {
                     </div> */}
                 </div>
                 <div className='clothing-element-frame'>
-                    <Button>Prev</Button>
+                    <Button disabled={disableBottomPrevButton} onClick={() => getPrevPhoto('bottom')}>Prev</Button>
                     <div className='bottoms-frame'>
                         <Frame w="100%" h="100%" bgColor="$material" boxShadow="$out" padding="$4">
-                          <img className='clothing-image' src={`http://localhost:5000/wardrobe/${currentBottom}`} alt="top" />
+                          <img className='clothing-image' src={`http://localhost:5000/wardrobe/${currentBottoms[currentBottomIndex]}`} alt="top" />
                         </Frame>
                     </div>
-                    <Button>Next</Button>
+                    <Button disabled={disableBottomNextButton} onClick={() => getNextPhoto('bottom')}>Next</Button>
                 </div>
                 
                 <div className='clothing-element-frame'>
-                    <Button>Prev</Button>
+                    <Button disabled={disableShoePrevButton} onClick={() => getPrevPhoto('shoe')}>Prev</Button>
                     <div className='shoes-frame'>
                     <Frame w="100%" h="100%" bgColor="$material" boxShadow="$out" padding="$4">
                         Shoes
                     </Frame>
                     </div>
-                    <Button>Next</Button>
+                    <Button disabled={disableShoeNextButton} onClick={() => getNextPhoto('shoe')}>Next</Button>
                 </div>
               </Frame>
               
--- frontend/main.tsx
+++ frontend/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import '@react95/core/GlobalStyle';
-import '@react95/core/themes/lilacRoseDark.css';
+import '@react95/core/themes/candy.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
